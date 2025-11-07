05fea41cbc648e95b6fbd99806ed2ba07baaf925
kamran sidiq <kamransidiq169@gmail.com>
2025-11-07 10:12:02 +0530

    fix db

--- ExpressUrl/drizzle/schema.js
+++ ExpressUrl/drizzle/schema.js
@@ -1,7 +1,7 @@
 
 import { relations, sql } from 'drizzle-orm';
 import { text } from 'drizzle-orm/gel-core';
-import { mysqlTable, serial, varchar, timestamp, int, boolean, bigint, mysqlEnum } from 'drizzle-orm/mysql-core';
+import { mysqlTable, serial, varchar, timestamp, int, boolean, bigint,mysqlEnum } from 'drizzle-orm/mysql-core';
 
 
 export const users = mysqlTable('RegisterUsers', {
@@ -16,7 +16,7 @@ export const users = mysqlTable('RegisterUsers', {
 
 export const oauthAccountsTable = mysqlTable("oauth_accounts",{
   id:int("id").autoincrement().primaryKey(),
-  userId:int('user_id').notNull().references(()=>users.id,{onDelete:"cascade"}),
+  userId:bigint("user_id", { mode: "number" }).notNull().references(()=>users.id,{onDelete:"cascade"}),
   password: varchar('password', { length: 255 }).default(sql`NULL`),
   provider:mysqlEnum("provider",["google","github"]).notNull(),
   providerAccountId:varchar("provider_account_id",{length:255}).notNull().unique(),
@@ -24,7 +24,7 @@ export const oauthAccountsTable = mysqlTable("oauth_accounts",{
 })
 export const passwordResetTokenTable = mysqlTable("password_reset_tokens", {
   id: int().autoincrement().primaryKey(),
-  userId: int("user_id").notNull().references(() => users.id, { onDelete: "cascade" }).unique(),
+  userId:bigint("user_id", { mode: "number" }).notNull().references(() => users.id, { onDelete: "cascade" }).unique(),
   tokenHash: text("token_hash").notNull(),
   expiresAt: timestamp("expires_at").default(sql`(CURRENT_TIMESTAMP + INTERVAL 1 HOUR)`).notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
@@ -32,7 +32,7 @@ export const passwordResetTokenTable = mysqlTable("password_reset_tokens", {
 
 export const verifyEmailTokensTable = mysqlTable("is_email_valid", {
   id: int().autoincrement().primaryKey(),
-  userId: int("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
+  userId:bigint("user_id", { mode: "number" }).notNull().references(() => users.id, { onDelete: "cascade" }),
   token: varchar("token", { length: 255 }).notNull(),
   expiresAt: timestamp("expires_at").default(sql`(CURRENT_TIMESTAMP + INTERVAL 1 DAY)`).notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
@@ -40,7 +40,7 @@ export const verifyEmailTokensTable = mysqlTable("is_email_valid", {
 
 export const sessionsData = mysqlTable("sessions", {
   id: serial('id').primaryKey().autoincrement(),
-  userId: bigint("user_id", {}).notNull().references(() => users.id, { onDelete: "cascade" }),
+  userId:bigint("user_id", { mode: "number" }).notNull().references(() => users.id, { onDelete: "cascade" }),
   valid: boolean().default(true).notNull(),
   userAgent: text("user_agent"), // is sai pata chalega ki user ne kis operating system mai login e.g windows etc
   ip: varchar({ length: 255 }),
@@ -54,7 +54,7 @@ export const shortLinks = mysqlTable("ShortLinks", {
   shortcode: varchar("shortcode", { length: 100 }).notNull().unique(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at').defaultNow().onUpdateNow().notNull(),
-  userId: int("users_id").notNull().references(() => users.id)
+  userId:bigint("user_id", { mode: "number" }).notNull().references(() => users.id)
 });
 
 // A user can have many shortlinks
