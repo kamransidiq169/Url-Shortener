6c63d64d35ce8e5cebd4103a3713e2fb9321b2cc
kamran sidiq <kamransidiq169@gmail.com>
2025-11-08 15:02:05 +0530

    fixed

--- ExpressUrl/lib/sendEmail.js
+++ ExpressUrl/lib/sendEmail.js
@@ -1,27 +1,50 @@
 
 
 // const resend = new Resend(process.env.RESEND_API)
-import dotenv from 'dotenv';
-dotenv.config({ path: './ExpressUrl/.env' }); // 👈 explicitly point to the correct location
+// import dotenv from 'dotenv';
+// dotenv.config({ path: './ExpressUrl/.env' }); // 👈 explicitly point to the correct location
+
+// import { Resend } from 'resend';
+// const resend = new Resend(process.env.RESEND_API);
+// export const sendEmail = async ({ to, subject, html }) => {
+//   try {
+//     const {data,error} = await resend.emails.send({
+//       from:"Website <website@resend.dev>",
+//       to:[to],
+//       subject,
+//       html,
+//     })
+//     if(error){
+//         return   console.error(error)
+//     }else{
+//         console.log(data);
+        
+//     }
+// }catch(error){
+//     console.error(error)
+// }
+
+// }
 
 import { Resend } from 'resend';
-const resend = new Resend(process.env.RESEND_API);
+
+const resend = new Resend(process.env.RESEND_API); // Railway injects this automatically
+
 export const sendEmail = async ({ to, subject, html }) => {
   try {
-    const {data,error} = await resend.emails.send({
-      from:"Website <website@resend.dev>",
-      to:[to],
+    const { data, error } = await resend.emails.send({
+      from: "Website <website@resend.dev>",
+      to: [to],
       subject,
       html,
-    })
-    if(error){
-        return   console.error(error)
-    }else{
-        console.log(data);
-        
-    }
-}catch(error){
-    console.error(error)
-}
+    });
 
-}
+    if (error) {
+      return console.error(error);
+    } else {
+      console.log(data);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
