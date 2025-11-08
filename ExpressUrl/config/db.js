893c200d8d3ce1402b8424682073e48f4575ffd0
kamran sidiq <kamransidiq169@gmail.com>
2025-11-08 20:39:12 +0530

    third commit

--- ExpressUrl/config/db.js
+++ ExpressUrl/config/db.js
@@ -1,18 +1,27 @@
 // import { drizzle } from "drizzle-orm/mysql2";
 // export const db = drizzle(process.env.DATABASE_URL);
 // ExpressUrl/config/db.js
+// import { drizzle } from "drizzle-orm/mysql2";
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const poolConnection = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT),
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// export const db = drizzle(poolConnection);
+
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const poolConnection = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-export const db = drizzle(poolConnection);
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
+export const db = drizzle(connection);
